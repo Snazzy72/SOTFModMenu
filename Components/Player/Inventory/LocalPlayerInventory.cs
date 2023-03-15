568923d10e0daef57dda743541ecd3f1333376da
Snazzy72 <69359713+Snazzy72@users.noreply.github.com>
2023-03-15 20:58:53 +0200

    Added `Components`

---
+++ Components/Player/Inventory/LocalPlayerInventory.cs
@@ -0,0 +1,28 @@
+﻿using Construction;
+using Il2CppSystem.Collections.Generic;
+using Il2CppSystem.IO;
+using Sons.Input;
+using Sons.Items.Core;
+using UnityEngine;
+using TheForest.Utils;
+using TheForest.Items.Inventory;
+using UnityEngine.SceneManagement;
+using Types = UnityEngine.Types;
+
+namespace SOTFModMenu.Components.Player.Inventory
+{
+    internal class LocalPlayerInventory
+    {
+        public static void AddItemToInventory(int itemID, int amount = 1)
+        {
+            try
+            {
+                LocalPlayer.Inventory.AddItem(itemID, amount);
+            }
+            catch
+            {
+                Plugin.Plugin.log.LogError($"Failed to add item to inventory! RefID: {itemID}, RefAmount: {amount}");
+            }
+        }
+    }
+}
---
+++ Components/Utility/CSV_TO_JSON_CONVERTER.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace SOTFModMenu.Components.Utility
+{
+    internal class CSV_TO_JSON_CONVERTER
+    {
+        static void ConvertCSV()
+        {
+            var csv = new List<string[]>();
+            var itemData = File.ReadAllLines("./ItemList.csv");
+
+            foreach (string item in itemData)
+            {
+                csv.Add(item.Split(','));
+            }
+
+            var itemProperties = itemData[0].Split(',');
+            var listObject = new List<Dictionary<string, string>>();
+
+            for (int i = 1; i < itemData.Length; ++i)
+            {
+                var objectResult = new Dictionary<string, string>();
+                for (int j = 0; j < itemProperties.Length; ++j)
+                {
+                    objectResult.Add(itemProperties[j], csv[i][j]);
+                }
+
+                listObject.Add(objectResult);
+            }
+
+            var json = JsonConvert.SerializeObject(listObject);
+            File.WriteAllText(@"./ItemList.json", json);
+        }
+    }
+}
